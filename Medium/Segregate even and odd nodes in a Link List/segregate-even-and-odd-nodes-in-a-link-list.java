dbc4db0b8561b96064e689d535b0eda6db3a3d46
Arkapratim Ghosh <arkapratimghosh2002@gmail.com>
2023-08-07 00:49:26 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Segregate even and odd nodes in a Link List/segregate-even-and-odd-nodes-in-a-link-list.java
@@ -0,0 +1,116 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+
+class FastReader{ 
+    BufferedReader br; 
+    StringTokenizer st; 
+
+    public FastReader(){ 
+        br = new BufferedReader(new InputStreamReader(System.in)); 
+    } 
+
+    String next(){ 
+        while (st == null || !st.hasMoreElements()){ 
+            try{ st = new StringTokenizer(br.readLine()); } catch (IOException  e){ e.printStackTrace(); } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    String nextLine(){ 
+        String str = ""; 
+        try{ str = br.readLine(); } catch (IOException e) { e.printStackTrace(); } 
+        return str; 
+    } 
+    
+    Integer nextInt(){
+        return Integer.parseInt(next());
+    }
+} 
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+class GFG{
+	static void printList(Node node, PrintWriter out) 
+	{ 
+		while (node != null) 
+		{ 
+			out.print(node.data + " "); 
+			node = node.next; 
+		} 
+		out.println(); 
+	}
+    public static void main(String args[]) throws IOException { 
+        FastReader sc = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+        int t = sc.nextInt();
+        while(t > 0){
+        	int n = sc.nextInt();
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            for(int i=0; i<n-1; i++)
+            {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+            Solution ob = new Solution();
+            Node ans = ob.divide(n, head);
+            printList(ans, out); 
+            t--;
+        }
+        out.flush();
+    } 
+} 
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/* 
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+*/
+
+class Solution{
+    Node divide(int N, Node head){
+        // code here
+        List<Integer>e=new ArrayList<>();
+        List<Integer>o=new ArrayList<>();
+        Node p=head;
+        while(p!=null)
+        {
+            if(p.data%2==0) e.add(p.data);
+            else o.add(p.data);
+            p=p.next;
+        }
+        e.addAll(o);
+        Node start=null;
+        start=new Node(e.get(0));
+        Node cur=start;
+        for(int i=1;i<e.size();i++)
+        {
+           Node temp=new Node(e.get(i));
+           cur.next=temp;
+           cur=temp;
+        }
+        return start;
+        
+    }
+ 
+}
