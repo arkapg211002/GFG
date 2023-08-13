6e31000c608b34e2ea06dcc696f3951186a053e5
Arkapratim Ghosh <arkapratimghosh2002@gmail.com>
2023-08-13 14:01:23 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Sum of subarray ranges/sum-of-subarray-ranges.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t;
+        t = sc.nextInt();
+        while (t-- > 0) {
+
+            int N;
+            N = sc.nextInt();
+
+            int[] arr = new int[N];
+            for (int i = 0; i < N; i++) arr[i] = sc.nextInt();
+
+            Solution obj = new Solution();
+            long res = obj.subarrayRanges(N, arr);
+
+            System.out.println(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+
+class Solution {
+    public static long subarrayRanges(int N, int[] arr) {
+        int res=0;
+        for(int i=0;i<N-1;i++){
+            int min=arr[i];
+            int max=arr[i];
+            for(int j=i+1;j<N;j++){
+                min=Math.min(min,arr[j]);
+                max=Math.max(max,arr[j]);
+                res+=(max-min);
+            }
+            
+        }
+        return res;
+        
+        
+    }
+}
