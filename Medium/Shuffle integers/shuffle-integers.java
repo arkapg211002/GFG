a62049945cd9975bcb42060214b342318f5b4179
Arkapratim Ghosh <arkapratimghosh2002@gmail.com>
2023-11-25 19:54:13 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Shuffle integers/shuffle-integers.java
@@ -0,0 +1,89 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+//Initial Template for Java
+
+
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+
+
+
+
+class  Solution
+{
+    void shuffleArray(long arr[], int n)
+    {
+        // Your code goes here
+        int len = n/2;
+	    for(int i=0; i<len; i++) {
+	        arr[i] = arr[i] << 10;
+	        arr[i] = arr[i] | arr[i+len]; //store x,y
+	    }
+	    
+	    int j=n-1;
+	    long p = (long)Math.pow(2, 10);
+	    for(int i=len-1; i>=0; i--) {
+	        long y = arr[i] & (p-1);
+	        long x = arr[i] >> 10;
+	        arr[j] = y;
+	        arr[j-1] = x;
+	        j -= 2;
+	    }
+    }
+}
+
+
+
+//{ Driver Code Starts.
+
+// Driver class
+class Array {
+
+    // Driver code
+    public static void main(String[] args) throws IOException {
+        // Taking input using buffered reader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testcases = Integer.parseInt(br.readLine());
+        // looping through all testcases
+        while (testcases-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+//            String line = br.readLine();
+//            String[] q = line.trim().split("\\s+");
+//            int n =Integer.parseInt(q[0]);
+//            int k =Integer.parseInt(q[1]);
+            //int y =Integer.parseInt(a2[2]);
+            String line1 = br.readLine();
+            String[] a1 = line1.trim().split("\\s+");
+            long a[] = new long[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(a1[i]);
+            }
+//            String line2 = br.readLine();
+//            String[] a2 = line2.trim().split("\\s+");
+//            long b[] = new long[n];
+//            for (int i = 0; i < m; i++) {
+//                b[i] = Long.parseLong(a2[i]);
+//            }
+             Solution ob = new  Solution();
+            //ArrayList<Long> ans=
+            ob.shuffleArray(a,n);
+           // System.out.println(ob.shuffleArray(a,n));
+            for (int i = 0; i < n; i++)
+                System.out.print(a[i]+" ");
+            System.out.println();
+        }
+    }
+}
+
+
+// } Driver Code Ends
