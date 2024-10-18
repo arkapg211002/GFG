2eecec0f0e81d27bbbc601adb70ab49ebcdff436
Arkapratim Ghosh <arkapratimghosh2002@gmail.com>
2024-10-18 21:40:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Quick Sort/quick-sort.java
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+import java.util.*;
+class Sorting
+{
+	static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i]+" ");
+        System.out.println();
+    }
+    
+    // Driver program
+    public static void main(String args[])
+    {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		while(T>0)
+		{
+			int n = sc.nextInt();
+			int arr[] = new int[n];
+			for(int i=0;i<n;i++)
+				arr[i] = sc.nextInt();
+
+			
+			new Solution().quickSort(arr,0,n-1);
+			printArray(arr);
+		    T--;
+		}
+} }
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to sort an array using quick sort algorithm.
+    static void quickSort(int arr[], int si, int ei)
+    {
+        // code here
+         if(si>=ei){
+            return;
+        }
+
+        int pivotIdx=partition(arr,si,ei);
+        quickSort(arr, si, pivotIdx-1);
+        quickSort(arr, pivotIdx+1,ei);
+        
+    }
+    static int partition(int arr[], int si, int ei)
+    {
+        // your code here
+        int pivot=arr[ei];
+        int i=si-1;
+        for(int j=si;j<ei;j++){
+            if(arr[j]<pivot){
+                i++;
+                int temp=arr[j];
+                arr[j]=arr[i];
+                arr[i]=temp;
+            }
+        }
+        i++;
+        int temp=pivot;
+        arr[ei]=arr[i];
+        arr[i]=temp;
+        return i;
+    } 
+}
+
+
+
