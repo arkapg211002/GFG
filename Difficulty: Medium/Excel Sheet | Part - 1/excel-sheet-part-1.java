8a5b4d79ecf29ffd52b269b51f1128b381eda7d1
Arkapratim Ghosh <arkapratimghosh2002@gmail.com>
2024-10-19 15:12:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Excel Sheet | Part - 1/excel-sheet-part-1.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main (String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	    int t=Integer.parseInt(br.readLine());
+    
+	    while(t > 0){
+	        int n = Integer.parseInt(br.readLine());
+	        Solution T = new Solution();
+	        
+	        System.out.println(T.excelColumn(n));
+	        
+            t--;
+	    }
+	}
+    
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution {
+    public String excelColumn(int N){
+       StringBuilder ans= new StringBuilder();
+       while(N!=0){
+           N--;
+           int mod = N%26;
+           ans.append((char)('A'+mod));
+           N=N/26;
+           
+       }
+        
+        return ans.reverse().toString();
+        
+    }
+}
