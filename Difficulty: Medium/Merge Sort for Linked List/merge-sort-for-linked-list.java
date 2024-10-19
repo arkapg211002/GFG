fba8296d7f98e76db3bc0e3df6802d3ee2c1c09a
Arkapratim Ghosh <arkapratimghosh2002@gmail.com>
2024-10-19 15:00:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Merge Sort for Linked List/merge-sort-for-linked-list.java
@@ -0,0 +1,92 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+
+// } Driver Code Ends
+/*
+The structure of linked list is the following
+
+class Node
+{
+int data;
+Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    // Function to sort the given linked list using Merge Sort.
+    static Node mergeSort(Node head) {
+        // add your code here
+        Node cur=head;
+        ArrayList<Integer>li=new ArrayList<>();
+        while(cur!=null){
+            li.add(cur.data);
+            cur=cur.next;
+        }
+        Collections.sort(li);
+        cur=head;
+        int i=0;
+        while(cur!=null){
+            cur.data=li.get(i++);
+            cur=cur.next;
+        }
+        return head;
+    }
+}
+
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while (t-- > 0) {
+            List<Integer> arr = new ArrayList<>();
+            String input = br.readLine().trim();
+            StringTokenizer st = new StringTokenizer(input);
+            while (st.hasMoreTokens()) {
+                arr.add(Integer.parseInt(st.nextToken()));
+            }
+
+            Node head = null;
+            if (!arr.isEmpty()) {
+                head = new Node(arr.get(0));
+                Node tail = head;
+                for (int i = 1; i < arr.size(); i++) {
+                    tail.next = new Node(arr.get(i));
+                    tail = tail.next;
+                }
+            }
+            head = new Solution().mergeSort(head);
+            printList(head);
+        }
+    }
+}
+
+// } Driver Code Ends
