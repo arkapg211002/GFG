93aeba86c5bb47605440625e5f1399d2ece4fd01
Arkapratim Ghosh <arkapratimghosh2002@gmail.com>
2024-10-21 11:16:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Next Greater Element in Circular Array/next-greater-element-in-circular-array.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+/*package whatever //do not write package name here */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter ot = new PrintWriter(System.out);
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-->0){
+		    int n = Integer.parseInt(br.readLine().trim());
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    long[] arr = new long[n];
+		    for(int i=0; i<n; i++)arr[i]=Long.parseLong(inputLine[i]);
+		    Solution ob = new Solution();
+		    long[] res = ob.nextLargerElement(arr, n);
+		    for (int i = 0; i < n; i++) 
+		        ot.print(res[i] + " ");
+		    ot.println();
+		}
+        ot.close();
+	}
+}
+
+
+
+// } Driver Code Ends
+
+
+// User Function Template for JAVA
+
+class Solution{
+    public static long[] nextLargerElement(long[] arr, int n) { 
+        // Your code here
+        Stack<Long> s = new Stack<>();
+        long a[] = new long[n];
+        Arrays.fill(a, -1);
+
+        for(int i=2*n-1; i>=0; i--) {
+            int idx = i%n;
+            while(!s.isEmpty() && s.peek()<=arr[idx]) {
+                s.pop();
+            }
+            if(!s.isEmpty()) {
+                a[idx] = s.peek();
+            }
+            s.push(arr[idx]);
+        }
+        return a;
+    } 
+}
