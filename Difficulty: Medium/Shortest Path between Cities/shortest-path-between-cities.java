bcdd101909261775992b8ac55e2401c6b9018b0e
Arkapratim Ghosh <arkapratimghosh2002@gmail.com>
2024-12-04 10:43:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Shortest Path between Cities/shortest-path-between-cities.java
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*; 
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0){
+        	String input_line[] = read.readLine().trim().split("\\s+");
+        	int x = Integer.parseInt(input_line[0]);
+        	int y = Integer.parseInt(input_line[1]);
+            Solution ob = new Solution();
+            System.out.println(ob.shortestPath(x,y));
+        
+System.out.println("~");
+}
+    } 
+} 
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution { 
+    int shortestPath( int x, int y){ 
+        int count=0;
+        while(x!=y){
+            if(x>y){
+                x/=2;
+                
+            }
+            else{
+                y/=2;
+            }
+            count++;
+        }
+        return count;
+    }
+}
